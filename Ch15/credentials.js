8061b69951032c75b1435ad7dca37a8924405c3e
diehard5566 <shihyao001@gmail.com>
2022-04-10 01:09:37 +0800

    Ch15

---
+++ Ch15/credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongo: 'mongodb+srv://john:74108520@nodeexpressproject.zjgly.mongodb.net/BookExample?retryWrites=true&w=majority',
+    redis: {
+        url: 'redis://:gV4sWNahdJKAmnxcCcagKeK3lcjnPk2N@redis-12085.c1.ap-southeast-1-1.ec2.cloud.redislabs.com:12085',
+    },
+    cookieSecret: 'coming strength nothing half',
+}
